c9d07758a138ead4e8edc37446019c50cf75d6a0
PlusPig <2504722047@qq.com>
2025-10-20 00:23:07 +0800

    期中实验

---
+++ lab_midterm/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/InitShader.cpp.obj"
+  "CMakeFiles/main.dir/InitShader.cpp.obj.d"
+  "CMakeFiles/main.dir/glad.c.obj"
+  "CMakeFiles/main.dir/glad.c.obj.d"
+  "CMakeFiles/main.dir/main.cpp.obj"
+  "CMakeFiles/main.dir/main.cpp.obj.d"
+  "libmain.dll.a"
+  "main.exe"
+  "main.exe.manifest"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
